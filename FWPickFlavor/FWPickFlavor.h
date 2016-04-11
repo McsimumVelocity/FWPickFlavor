64996890ce55612aab17f49a159f2c3c0ae3da8b
Mike McNamara <mikemcnamara@tapadoo.com>
2016-04-11 15:51:31 +0100

    Initial Commit

---
+++ FWPickFlavor/FWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  FWPickFlavor.h
+//  FWPickFlavor
+//
+//  Created by Mike McNamara on 11/04/2016.
+//  Copyright © 2016 Mike McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FWPickFlavor.
+FOUNDATION_EXPORT double FWPickFlavorVersionNumber;
+
+//! Project version string for FWPickFlavor.
+FOUNDATION_EXPORT const unsigned char FWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWPickFlavor/PublicHeader.h>
+
+
